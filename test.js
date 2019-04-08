546b536654f286c743f9cb5c696b1c660d283656
Anden Acitelli <a.acitelli@gmail.com>
2019-04-07 23:51:37 -0400

    Implemented better stats reporting

--- test.js
+++ test.js
@@ -8,15 +8,34 @@ fs.readdir(__dirname, (err, files) =>
 
     files.forEach((file) =>
     {
-        let fileName = directoryPath + "\\" + file;
+        let filePath = __dirname + "\\" + file;
 
         // Opens the file itself from the directory
-        fs.readFile(fileName, 'utf8', (err2, data) =>
+        fs.stat(filePath, (err, stats) =>
         {
             if (err) console.error(err);
 
-            console.log("Data: ");
-            console.log(data);
+            displayLine(filePath, stats); 
         });
     });
 });
+
+function displayLine(fileName, stats)
+{
+    console.log("File Name: " + getFileNameFromFilePath(fileName));
+    console.log("File Stats: ");
+    console.log(stats);
+
+    console.log("--------------------");
+}
+
+function getFileNameFromFilePath(filePath)
+{
+    // Case where it doesn't have a parent directory (doubt this is possible, but covered nonetheless)
+    if (filePath.lastIndexOf("\\") == -1)
+    {
+        return filePath;
+    }
+
+    return filePath.substring(filePath.lastIndexOf("\\") + 1, filePath.length);
+}
